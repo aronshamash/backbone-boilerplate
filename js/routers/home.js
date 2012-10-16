acd5e6ac9faa7052649169560c25086bafe66d1e
Aron Shamash <aron.shamash@globaldawn.co>
2012-10-16 10:54:20 +0100

    test commit

--- js/routers/home.js
+++ js/routers/home.js
@@ -10,11 +10,23 @@ function($, Backbone, _, mainView){
 			Backbone.history.start();
 		},
 		routes: {
-			'': 'home'
+			'': 'home',
+            'css3': 'css3',
+            'js': 'js',
+            'node': 'node'
 		},
 		'home': function(){
 			this.mainView.render();
-		}
+		},
+        'css3': function(){
+            this.mainView.render();
+        },
+        'js': function(){
+            this.mainView.render();
+        },
+        'node': function(){
+            this.mainView.render();
+        }
 	});
 	
 	return Router;
